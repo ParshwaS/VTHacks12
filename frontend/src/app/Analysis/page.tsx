34d8dd098ed41551a359ab0a503a820c04d48122
Foram <shahforam2002@gmail.com>
2024-09-15 04:23:12 -0400

    map view integratedto analysis

--- frontend/src/app/Analysis/page.tsx
+++ frontend/src/app/Analysis/page.tsx
@@ -5,35 +5,40 @@ import LineChart from '@/app/LineChart1/page';
 import Heatmap from '@/app/Heatmap/page';
 import Correlation from '../corelation/page';
 import PricesByTimeChart from '../pricesqtime/page';
+import MapView from '@/components/MapView';
 // import correlation from '@/app/corelation'; // Assume another graph component
 // import PieChart from '@/components/graphs'; // Assume another graph component
 
 const GraphsPage = () => {
   return (
     <div className="container font-sans m-auto p-4">
-      <div className="flex mb-4 gap-4">
+      <div className="grid mb-4 gap-4 grid-cols-12">
         {/* Card 1: Line Chart */}
-        <div className="card bg-white p-4 shadow-md rounded-lg">
+        <div className="card bg-white p-4 col-span-8 shadow-md rounded-lg" style={{"width":"100%"}}>
           <h2 className="text-xl font-semibold mb-2">Avg. Rental Price over Time</h2>
-          <p></p>
           <LineChart />
+        </div>
+        <div className="card bg-white p-4 col-span-4 shadow-md rounded-lg">
+          <h2 className="text-xl font-semibold mb-2">Search by Zipcode</h2>
+          <MapView />
+          <p></p>
         </div>
       </div>
 
-      <div className="flex mb-4 gap-4">
+      <div className="grid my-4 grid-cols-12 gap-4">
         {/* Card 1: Line Chart */}
-        <div className="card bg-white p-4 shadow-md rounded-lg">
+        <div className="card bg-white p-4 col-span-7 shadow-md rounded-lg">
           <h2 className="text-xl font-semibold mb-2"></h2>
           <Correlation />
         </div>
          {/* Card 2: Heatmap */}
-         <div className="card flex flex-col justify-center items-center align-middle bg-white p-4 shadow-md end-0 rounded-lg">
+         <div className="card flex flex-col justify-center col-span-5 items-center align-middle bg-white p-4 shadow-md end-0 rounded-lg">
           <h2 className="text-xl font-semibold mb-2">Percentage of housing units in Proximity</h2>
           <Heatmap />
         </div>
       </div>
 
-      <div className="flex mb-4 gap-4">
+      <div className="flex my-4 gap-4">
         {/* Card 2: Heatmap */}
         <div className="card flex flex-col justify-center items-center align-middle bg-white p-4 shadow-md end-0 rounded-lg">
           <h2 className="text-xl font-semibold mb-2">Price per Square Feet over Time</h2>
